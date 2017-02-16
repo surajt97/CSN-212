50fe8ee7a36be4f3c36e2590ce478a52da7c3275
surajt97 <suraj.t97@gmail.com>
2017-02-16 22:45:26 +0530

    deletion method updated

--- TUT-3(Interval Tree)/IntervalTree.cpp
+++ TUT-3(Interval Tree)/IntervalTree.cpp
@@ -7,7 +7,7 @@ int max(int a, int b, int c){
 }
 struct node{
     int low, high, maxh;
-    node *left, *right, *parent;
+    node *left, *right;
 };
 class IntervalTree{
 public:
@@ -22,7 +22,7 @@ public:
             root->low = l;
             root->high = h;
             root->maxh = h;
-            root->left = root->right = root->parent = NULL;
+            root->left = root->right = NULL;
         }
         else{
             node *p = root;
@@ -52,7 +52,6 @@ public:
                 p->left = q;
             }
             q->left = q->right = NULL;
-            q->parent = p;
         }
     }
     node* minValueNode(node* root){
@@ -61,35 +60,6 @@ public:
             p = p->left; 
         return p;
     }
-    node* deleteNode(node* root, int key){
-    if (root == NULL) 
-        return root;
-    if (key < root->low)
-        root->left = deleteNode(root->left, key);
-    else if (key > root->low)
-        root->right = deleteNode(root->right, key);
-    else{
-        node *p = root->parent;
-        int maxleft = (root->left)->maxh, maxrt = (root->right)->maxh;
-        if (root->left == NULL){
-            node *temp = root->right;
-            p->maxh = max(p->maxh, temp->maxh);
-            return temp;
-        }
-        else if (root->right == NULL){
-            node *temp = root->left;
-            p->maxh = max(p->maxh, temp->maxh);        
-            return temp;
-        }
-        p->maxh = max(p->maxh, (root->left)->maxh, (root->right)->maxh);
-        node* temp = minValueNode(root->right);
-        root->low = temp->low;
-        root->high = temp->high;
-        root->maxh =  max(maxleft, maxrt);
-        root->right = deleteNode(root->right, temp->low);
-    }
-    return root;
-}
     node *search(node *root, int l, int h){
         if (root == NULL)
             return NULL;
@@ -98,6 +68,88 @@ public:
         if (root->left != NULL && (root->left)->maxh >= l)
             return search(root->left, l, h);
         return search(root->right, l, h);
+    }
+    void remove(node* p, node* root, node* parent, bool head){
+        int child = 0;
+        bool left = false, right = false;
+        if(p->low <= parent->low) 
+            left = true;
+        else 
+            right = true;
+        if(p->left) 
+            child++;
+        if(p->right)
+            child++;
+
+        if(child==0){
+            if(head){
+                parent = NULL;
+                return;
+            }
+            if(left)
+                parent->left = NULL;
+            else 
+                parent->right = NULL;
+        }
+        else if(child==1){
+            if(head){
+                if(p->left) 
+                    root = root->left;
+                else 
+                    root = root->right;
+                return;
+            }
+            if(left){
+                if(p->right) 
+                    parent->left = p->right;
+                else 
+                    parent->left = p->left;
+            }
+            else{
+                if(p->right) 
+                    parent->right = p->right;
+                else 
+                    parent->right = p->left;
+            }
+        }
+        else{
+            node* temp = p;
+            temp = temp->left;
+            if(temp->right){
+                while(temp->right){
+                    parent = temp;
+                    temp = temp->right;
+                }
+                p->low = temp->low;
+                p->high = temp->high;
+                parent->right = NULL;
+            }
+            else{
+                p->low = temp->low;
+                p->high = temp->high;
+                remove(temp, root, p, false);
+            }
+        }
+    }
+    node* deletenode(node* root, node* temp){
+        node* p = new node();
+        p = root;
+        node *parent= new node();
+        parent = root;
+        while(true){
+            if(temp->low < p->low){
+                parent = p; 
+                p = p->left;
+            }
+            else if(temp->low > p->low){
+                parent = p; 
+                p = p->right;
+            }
+            else{
+                remove(p, root, parent, false);
+                break;
+            }
+        }
     }
 };
 int main(){
@@ -122,5 +174,14 @@ int main(){
     else{
         cout<<"No overlap interval found"<<endl;
     }
+    //deletion
+    node *d = new node();
+    cin>>l>>h;
+    d->low = l;
+    d->high = h;
+    d->left = d->right = NULL;
+    d->maxh = h;
+    t.deletenode(t.root, d);
+    cout<<"Node deleted"<<endl;
     return 0;
 }
